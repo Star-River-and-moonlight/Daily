274e2f403ac70ea698d027421ed16bbd29c8b29b
Star-River-and-moonlight <3323242047@qq.com>
2023-03-07 20:39:01 +0800

    two

---
+++ rendering.js
@@ -0,0 +1,11 @@
+window.onload = function () {
+    var button1 = document.getElementById("button1");
+    button1.onclick = function () {
+        alert("爱笑的人运气都不差😊");
+    }
+
+    var button2 = document.getElementById("button2");
+    button2.onclick = function () {
+        alert("不以今之悲伤，染至明之心情✨")
+    }
+}
